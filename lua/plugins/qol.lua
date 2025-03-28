d8535a94121d26a9a7ea99a83c0dad4c1ee86300
Kien T. Nguyen <tukidev63@gmail.com>
2025-03-28 23:47:48 +0800

    add whichkey for regs only

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -88,5 +88,34 @@ return {
         event = 'BufReadPre',
         opts = {
         },
+    },
+    {
+        'folke/which-key.nvim',
+        event = 'VeryLazy',
+        opts = {
+            preset = 'classic',
+            delay = function (ctx)
+                return ctx.plugin and 0 or 500
+            end,
+            triggers = {
+                { '\"',    mode = 'nv' },
+                { '<c-r>', mode = 'i' },
+            },
+            plugins = {
+                marks = false,
+                registers = true,
+                spelling = false,
+                presets = false,
+            }
+        },
+        keys = {
+            {
+                '<leader>?',
+                function ()
+                    require('which-key').show({ global = false })
+                end,
+                desc = 'Buffer Local Keymaps (which-key)',
+            },
+        },
     }
 }
