ad4e51d705663fd9de6adeef69b39c8969ae781b
Kien T. Nguyen <kiennt603@gmail.com>
2026-05-17 17:06:49 +0800

    fix: make aerial buffer sticky

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -3,7 +3,7 @@ return {
         'ThePrimeagen/harpoon',
         branch = 'harpoon2',
         dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
+        config = function ()
             local harpoon = require 'harpoon'
             local ag = vim.api.nvim_create_augroup
             local au = vim.api.nvim_create_autocmd
@@ -14,32 +14,32 @@ return {
             au('FileType', {
                 group = ag('HarpoonMenu', {}),
                 pattern = 'harpoon',
-                callback = function()
-                    vim.keymap.set('n', '1', function()
+                callback = function ()
+                    vim.keymap.set('n', '1', function ()
                         harpoon:list():select(1)
                     end, { silent = true, buffer = true })
-                    vim.keymap.set('n', '2', function()
+                    vim.keymap.set('n', '2', function ()
                         harpoon:list():select(2)
                     end, { silent = true, buffer = true })
-                    vim.keymap.set('n', '3', function()
+                    vim.keymap.set('n', '3', function ()
                         harpoon:list():select(3)
                     end, { silent = true, buffer = true })
-                    vim.keymap.set('n', '4', function()
+                    vim.keymap.set('n', '4', function ()
                         harpoon:list():select(4)
                     end, { silent = true, buffer = true })
-                    vim.keymap.set('n', '5', function()
+                    vim.keymap.set('n', '5', function ()
                         harpoon:list():select(5)
                     end, { silent = true, buffer = true })
-                    vim.keymap.set('n', '6', function()
+                    vim.keymap.set('n', '6', function ()
                         harpoon:list():select(6)
                     end, { silent = true, buffer = true })
-                    vim.keymap.set('n', '7', function()
+                    vim.keymap.set('n', '7', function ()
                         harpoon:list():select(7)
                     end, { silent = true, buffer = true })
-                    vim.keymap.set('n', '8', function()
+                    vim.keymap.set('n', '8', function ()
                         harpoon:list():select(8)
                     end, { silent = true, buffer = true })
-                    vim.keymap.set('n', '9', function()
+                    vim.keymap.set('n', '9', function ()
                         harpoon:list():select(9)
                     end, { silent = true, buffer = true })
                 end,
@@ -48,6 +48,22 @@ return {
     },
     {
         'stevearc/aerial.nvim',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-tree/nvim-web-devicons',
+            'stevearc/stickybuf.nvim',
+        },
+        config = function (_, opts)
+            require('stickybuf').setup()
+            require('aerial').setup(opts)
+
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = 'aerial',
+                callback = function ()
+                    require('stickybuf').pin(0)
+                end,
+            })
+        end,
         opts = {
             layout = {
                 default_direction = 'left',
@@ -56,10 +72,6 @@ return {
                 max_width = { 40, 0.2 },
                 min_width = { 35, 0.25 },
             },
-        },
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter',
-            'nvim-tree/nvim-web-devicons',
         },
     },
 }
